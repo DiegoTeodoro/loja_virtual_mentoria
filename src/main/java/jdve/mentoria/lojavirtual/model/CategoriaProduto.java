6de9798dceadcf27d5c23e1a36f5b432bfc1c7bb
DiegoTeodoro <diegoteodoro_alves@yahoo.com.br>
2023-08-11 15:11:38 -0300

    Classe categoria produto seq

--- src/main/java/jdve/mentoria/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/jdve/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -13,13 +13,13 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="categoria_produto")
-@SequenceGenerator(name="seq_categoria_produto", sequenceName = "categoria_produto", allocationSize = 1, initialValue = 1)
+@SequenceGenerator(name="seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
 public class CategoriaProduto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
 	
 	@Column(name = "nome_desc", nullable = false)
